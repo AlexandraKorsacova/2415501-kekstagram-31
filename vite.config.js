184bd594869616dafb4696e47e2acd2763d242cf
Александра <Denusheva.Al@mail.ru>
2024-05-06 14:13:36 +0700

    ci(vite): try split vendor plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [splitVendorChunkPlugin()],
   base: '/2415501-kekstagram-31/',
 });
